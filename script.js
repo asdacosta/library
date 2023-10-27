a105e646b16ff6a9bcd378c8f112ef6cdffbce0f
asdacosta <asdacosta18@gmail.com>
2023-10-27 11:05:05 +0000

    Display form upon button click

---
+++ script.js
@@ -0,0 +1,14 @@
+const displayForm = document.querySelector('.displayForm');
+const dialog = document.querySelector('dialog');
+const hideForm = document.querySelector('.hideForm');
+
+displayForm.addEventListener('click', () => {
+    dialog.showModal();
+})
+
+
+// const myLibrary = [];
+
+// function addBookToLibrary () {
+
+// }
