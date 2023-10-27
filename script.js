e780e4ac5af8278fd5b276b402edabe507d94ea9
asdacosta <asdacosta18@gmail.com>
2023-10-27 17:24:40 +0000

    Create fxns to take input values from form and append to library array

--- script.js
+++ script.js
@@ -8,11 +8,42 @@ displayForm.addEventListener('click', () => {
 
 hideForm.addEventListener('click', () => {
     dialog.close();
+    addBookToLibrary();
 })
 
 
-// const myLibrary = [];
+const myLibrary = [];
 
-// function addBookToLibrary () {
+function Book (author, title, pages, read) {
+    this.author = author,
+    this.title = title,
+    this.pages = pages,
+    this.read = read
+}
 
-// }
+for (n = 0; n < 20; n++) {
+    const book = new Book();
+    myLibrary.push(book);
+}
+
+const firstOption = document.querySelector('#read');
+const secOption = document.querySelector('#not-read');
+let starter = 0;
+
+function addBookToLibrary () {
+    const authorValue = document.querySelector('#author');
+    const titleValue = document.querySelector('#title');
+    const pagesValue = document.querySelector('#num');
+
+    if (firstOption.checked) {
+        const readValue = firstOption.value;
+    } else if (secOption.checked) {
+        const readValue = secOption.value;
+    }
+
+    myLibrary[starter].author = authorValue;
+    myLibrary[starter].title = titleValue;
+    myLibrary[starter].pages = pagesValue;
+    myLibrary[starter].read = readValue;
+    starter += 1;
+}
